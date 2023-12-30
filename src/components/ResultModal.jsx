3a3fd750826c957c845e9b1922e1547e9b89ebd0
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2023-12-31 01:14:52 +0530

    add socre and handle ESC key press

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -8,6 +8,7 @@ const ResultModal = forwardRef(function ResultModal(
 
   const userLost = timeRemaining <= 0;
   const timeLeft = (timeRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - timeRemaining / (targetTimer * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -18,15 +19,16 @@ const ResultModal = forwardRef(function ResultModal(
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost ☹️</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTimer}</strong> seconds.
       </p>
       <p>
         You stopped the timer with <strong>{timeLeft} seconds left.</strong>
       </p>
-      <form method="dialog" onSubmit={onReset}>
+      <form method="dialog">
         <button>CLose</button>
       </form>
     </dialog>
