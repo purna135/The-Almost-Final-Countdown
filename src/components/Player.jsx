8da2fc07abb07cd13174c1c6dbe06855c334f492
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2023-12-30 23:00:24 +0530

    add useState to manage player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState();
+  const [submit, setSubmit] = useState(false);
+
+  function handleChange(event){
+    setSubmit(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleSubmit(){
+    setSubmit(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submit ? playerName: 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName}/>
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
